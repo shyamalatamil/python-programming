5c9d9b37a533f6703f3ce29eea4e4ff00a035c81
shyamalatamil <36620052+shyamalatamil@users.noreply.github.com>
2018-02-20 19:03:38 +0530

    Update vowel or consonant.py

--- beginner level/vowel or consonant.py
+++ beginner level/vowel or consonant.py
@@ -3,6 +3,6 @@ def main():
 
  if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
  or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-    print( "is a Vowel")
+    print( "Vowel")
  else:
-    print("is a Consonant")
+    print("Consonant")
