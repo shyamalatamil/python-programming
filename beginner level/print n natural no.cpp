bcff6b73d85b03b32dba8a2724775a7f46576af4
shyamalatamil <36620052+shyamalatamil@users.noreply.github.com>
2018-02-21 18:31:59 +0530

    Create print n natural no.cpp

---
+++ beginner level/print n natural no.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int no,i;
+int sum=0;
+    cin >> no;
+    for (i=1; i<=no; i++) {
+        sum=sum+i;
+    }	
+    cout<<sum;
+    return 0;
+}
