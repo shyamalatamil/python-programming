8001e380c08a39addd01259152cf65bc11857a98
shyamalatamil <36620052+shyamalatamil@users.noreply.github.com>
2018-02-26 20:09:40 +0530

    Create power of given no.java

---
+++ beginner level/power of given no.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner data = new Scanner(System.in);
+int a, b; 
+double c;
+a=data.nextInt();
+b=data.nextInt();
+c=Math.pow(a,b);
+System.out.println(c);
+	}
+}
